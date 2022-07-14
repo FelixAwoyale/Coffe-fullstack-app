b6c8ebcac807146a3a4ca9a245efb76bc814e0ee
FelixYale <dexteryale@gmail.com>
2022-07-14 13:58:01 +0100

    updates

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'dev-updrqncw.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'https://coffeeapp/'
 
 ## AuthError Exception
 '''
